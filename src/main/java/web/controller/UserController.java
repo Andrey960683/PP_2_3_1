1287d58d82087721ff132d0cc433fe46dbf2a4d9
Andrey Sidorov <andrey960683@gmail.com>
2024-10-20 22:46:03 +0300

    1.2

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,9 +3,12 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -31,12 +34,15 @@ public class UserController {
     }
 
     @GetMapping("/new")
-    public String newUser(){
+    public String newUser(@ModelAttribute("user") User user){
         return "users/new";
     }
 
     @PostMapping("/new")
-    public String create(@ModelAttribute("user") User user){
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "users/new";
+        }
         userService.addUser(user);
         return "redirect:/users";
     }
@@ -48,7 +54,10 @@ public class UserController {
     }
 
     @PostMapping("/edit")
-    public String update(@ModelAttribute("user") User user){
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "users/edit";
+        }
         userService.editUser(user);
         return "redirect:/users";
     }
